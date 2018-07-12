b50d72812eec125c9b6cc92e3c4c61e6cff1247d
Unknown <dchaika@inbox.ru>
2018-07-12 18:47:02 +0300

    slider functional has done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -27,16 +27,107 @@ $(document).ready(function () {
         slidesToShow: 1
     });
 
+    // Banner slider btns next / prev
+
+    var countClickNext = 0;
+    var countClickPrev = 0;
+
+    function showBtnsDots() {
+        if ( countClickNext ) {
+            $('.slider_dots-next').addClass('show')
+        } else if (!countClickNext) {
+            $('.slider_dots-next').removeClass('show')
+        }
+
+        if ( countClickPrev ) {
+            $('.slider_dots-prev').addClass('show')
+        } else if (!countClickPrev) {
+            $('.slider_dots-prev').removeClass('show')
+        }
+
+        if ( $(window).width() > 1440 ) {
+            $('.slider_dots-prev').removeClass('show');
+            $('.slider_dots-next').removeClass('show')
+        }
+    }
+
+    var countRounds = function () {
+        $('.layout__slider_dots').css({
+            'left': '0',
+            'right': 'auto'
+        });
+
+        countClickPrev = 0;
+
+        var widthBoxScrollBody = $('.layout__banner .layout__slider_wrap').width();
+        var lengthBoxScrollBody = $('.layout__slider_dots .dot-wrap').length;
+        var countShownBlocks = Math.floor(widthBoxScrollBody/230);
+
+        countClickNext = lengthBoxScrollBody - countShownBlocks;
+        console.log(countClickPrev, countClickNext, widthBoxScrollBody, lengthBoxScrollBody, countShownBlocks);
+    };
+
+    $('.slider_dots-next').on('click', function (e) {
+        e.preventDefault();
+
+        if ( countClickNext == 1 ) {
+            $('.layout__slider_dots').animate({'right': '0'}).css('left', 'auto');
+            countClickNext--;
+            countClickPrev++;
+        } else if ( countClickNext > 0 ) {
+            $('.layout__slider_dots').animate({'left': '-=230'});
+
+            countClickNext--;
+            countClickPrev++;
+        } else {
+            return false
+        }
+        console.log(countClickNext, countClickPrev);
+    });
+
+    $('.slider_dots-prev').on('click', function (e) {
+        e.preventDefault();
+
+        if ( countClickPrev == 1 ) {
+            $('.layout__slider_dots').animate({'left': '0'}).css('right', 'auto');
+            countClickNext++;
+            countClickPrev--;
+        } else if ( countClickPrev > 0 ) {
+            $('.layout__slider_dots').animate({'right': '-=230'});
+
+            countClickNext++;
+            countClickPrev--;
+        } else {
+            return false
+        }
+        console.log(countClickNext, countClickPrev);
+    });
+
+    $(window).on('load', function () {
+        if ( $(window).width() < 1440 ) countRounds();
+        showBtnsDots()
+    });
+
+    $(window).on('resize', function () {
+        if ( $(window).width() < 1440 ) countRounds();
+        showBtnsDots()
+    });
+
+    $('.slider_dots-prev, .slider_dots-next').on('click', function () {
+        showBtnsDots()
+    });
+
+
     // Slider btns
 
     $('.slider-prev').click(function () {
-       $(this).parent().prev('.layout__slider_content').find('.slick-prev').click();
+       $(this).parent().parent().find('.layout__slider_content').find('.slick-prev').click();
         changeActive();
 
     });
 
     $('.slider-next').click(function () {
-        $(this).parent().prev('.layout__slider_content').find('.slick-next').click();
+        $(this).parent().parent().find('.layout__slider_content').find('.slick-next').click();
         changeActive();
     });
 
